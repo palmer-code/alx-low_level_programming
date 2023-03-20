79a329a8cfd1fed40d4469a5269bd182344bbf5d
I.wahinya <56807127+palmer-code@users.noreply.github.com>
2023-03-20 23:39:48 +0300

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 
-/**
- * main - Prints the name of the file that the program was compiled from,
- *        followed by a new line.
- *
- */
+
 int main(void)
 {
 	printf("%s\n", __FILE__);
