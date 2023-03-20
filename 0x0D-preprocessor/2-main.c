2b8be98932cf406a080466ca7f3ee02d0cd0488c
I.wahinya <56807127+palmer-code@users.noreply.github.com>
2023-03-20 23:43:59 +0300

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main - Prints the name of the file that the program was compiled from,
+ *        followed by a new line.
+ *
+ * Return: Always 0.
+ *
+ * Author: Silas Mugambi.
+ */
 
 int main(void)
 {
