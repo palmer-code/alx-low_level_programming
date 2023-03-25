b3d9af18721e011735e57edb86aba9bfede3f053
I.wahinya <56807127+palmer-code@users.noreply.github.com>
2023-03-25 04:45:00 +0300

    Update 3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,29 +1,15 @@
-#ifndef CALC_H
-#define CALC_H
+#ifndef FUNCTION_POINTER_H
+#define FUNCTION_POINTER_H
 
 /*
- * Desc: Header file containing all structures and
- *       prototypes used by the 3-main.c program.
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0E-function_pointers directory.
  */
 
-/**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
- */
+#include <stdlib.h>
 
-typedef struct op
-{
-	char *op;
-	int (*f)(int a, int b);
-} op_t;
-
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
