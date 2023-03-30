2c43a9e80045e502e8a7a54b0a95590e3395afb6
I.wahinya <56807127+palmer-code@users.noreply.github.com>
2023-03-30 22:31:31 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,24 +2,25 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a linked list
- * @h: pointer to the list_t list to print
+ * print_list - prints all the elements of a list
+ * @h: pointer to the head of the list
  *
- * Return: No of nodes printed
+ * Return: the number of nodes
  */
+
 size_t print_list(const list_t *h)
 {
-	size_t s = 0;
+	size_t count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		if (!h->str)
-			printf("[0] (nil)\n");
-		else
-			printf("[%u] %s\n", h->len, h->str);
-		h = h->next;
-		s++;
-	}
+		if (h->str == NULL)
+		printf("[0] (nil)\n");
 
-	return (s);
+		else
+		printf("[%d] %s\n", h->len, h->str);
+	count++;
+	h = h->next;
+	}
+	return (count);
 }
