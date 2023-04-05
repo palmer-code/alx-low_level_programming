882872cb8de45ea49af24659e43bacfd9103cc4b
I.wahinya <56807127+palmer-code@users.noreply.github.com>
2023-04-05 21:55:11 +0300

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,33 +1,35 @@
-#include "lists.h"
-#include <stdio.h>
+#ifndef LISTS_H
+#define LISTS_H
+
 #include <stdlib.h>
 
 /**
- * free_listint_safe - frees a list
- * @h: pointer to head pointer
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
  *
- * Return: size of the list that was free’d
+ * Description: singly linked list node structure
+ *
  */
-
-size_t free_listint_safe(listint_t **h)
+typedef struct listint_s
 {
-	listint_t *current, *nxt;
-	size_t count = 0;
+	int n;
+	struct listint_s *next;
+} listint_t;
 
-	if (h == NULL || *h == NULL)
-		return (0);
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+void free_listint(listint_t *head);
+void free_listint2(listint_t **head);
+int pop_listint(listint_t **head);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+int sum_listint(listint_t *head);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
 
-	current = *h;
-	*h = NULL;
-
-	while (current != NULL)
-	{
-		nxt = current->next;
-		free(current);
-		count++;
-		if (nxt >= current)
-			break;
-		current = nxt;
-	}
-	return (count);
-}
+#endif
