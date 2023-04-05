54e71f83152d9ec20398bfa6001fcf1520ba193b
I.wahinya <56807127+palmer-code@users.noreply.github.com>
2023-04-05 22:43:21 +0300

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,45 +1,45 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - deletes the node at index
- * @head: head pointer
- * @index: index of node to be deleted
+ * delete_nodeint_at_index - Deletes the node at a given
+ *                           index of a listint_t list.
+ * @head: A pointer to the address of the
+ *        head of the listint_t list.
+ * @index: The index of the node to be deleted - indices start at 0.
  *
- * Return: 1 if successful, -1 if failed
+ * Return: On success - 1.
+ *         On failure - -1.
  */
-
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-    listint_t *current, *prev;
+	listint_t *current, *prev;
 
-    if (head == NULL || *head == NULL)
-        return (-1);
+	if (head == NULL || *head == NULL)
+		return (-1);
 
-    current = *head;
-    prev = *head;
+	current = *head;
+	prev = *head;
 
-    if (current == NULL)
-        return (-1);
-
-    else if (index == 0)
-    {
-        *head = current->next;
-        free(current);
-        return (1);
-    }
-    else
-    {
-        while (index != 0)
-        {
-            prev = current;
-            current = current->next;
-            index--;
-            if (current == NULL)
-                return (-1);
-        }
-        prev->next = current->next;
-        free(current);
-        return (1);
-    }
+	if (current == NULL)
+		return (-1);
+	else if (index == 0)
+	{
+		*head = current->next;
+		free(current);
+		return (1);
+	}
+	else
+	{
+		while (index != 0)
+		{
+			prev = current;
+			current = current->next;
+			index--;
+			if (current == NULL)
+				return (-1);
+		}
+		prev->next = current->next;
+		free(current);
+		return (1);
+	}
 }
-
